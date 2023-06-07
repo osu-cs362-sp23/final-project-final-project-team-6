d6abafa636c5248fab8607b67e67a6f86f1867df
KieranHostetlerMcLaughlin <92497265+KieranHostetlerMcLaughlin@users.noreply.github.com>
2023-06-07 11:20:21 -0700

    wrote generateChartImg test and tests for chartStorage

--- sortPoints.test.js
+++ sortPoints.test.js
@@ -5,3 +5,10 @@ test("Sort points", function() {
     const output = sortPoints(inputs)
     expect(output).toStrictEqual([{"x": 6, "y": 2}, {"x": 8, "y": 2}])
 })
+
+
+test("Sort points", function() {
+    const inputs = [{x: 6, y: 2}, {x: 8, y: 2}]
+    const output = sortPoints(inputs)
+    expect(output).toStrictEqual([{"x": 6, "y": 2}, {"x": 8, "y": 2}])
+})
